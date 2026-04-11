a23aebd7cfd068f2c9fa4a961479524b8faf7777
Ross Armstrong <hello@decibri.com>
2026-04-11 11:50:10 +1000

    Add Linux ARM64 platform + v3.0.0-rc.1 pre-flight

--- npm/decibri/src/browser/decibri-browser.js
+++ npm/decibri/src/browser/decibri-browser.js
@@ -3,7 +3,7 @@
 const { Emitter } = require('./emitter.js');
 const { WORKLET_SOURCE } = require('./worklet-inline.js');
 
-const VERSION = '3.0.0';
+const VERSION = '3.0.0-rc.1';
 
 /**
  * Browser microphone capture.
